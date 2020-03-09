11781a52b9effb15dbc78fbd2b069092551e6f2e
Treinamento <Treinamento@RD723900.raiadrogasil.com>
2020-03-09 16:48:31 -0300

    atualizando

---
+++ src/main/java/br/com/rd/ecommerce/Teste.java
@@ -0,0 +1,13 @@
+package br.com.rd.ecommerce;
+
+import br.com.rd.ecommerce.user.model.User;
+
+public class Teste {
+    User user = new User();
+
+    public static void main(String[] args) {
+        User user = new User(2L, "João","Silva", 18);
+
+
+    }
+}
---
+++ src/main/java/br/com/rd/ecommerce/user/model/User.java
@@ -0,0 +1,33 @@
+package br.com.rd.ecommerce.user.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "ds_first_name")
+    private String firstName;
+
+    @NotNull
+    @Column(name = "ds_last_name")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "nr_age")
+    private Integer age;
+
+}
